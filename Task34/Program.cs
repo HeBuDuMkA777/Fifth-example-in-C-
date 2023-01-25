dbdab2940fccc9e44d866229811824c9d2e79f4f
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-25 15:41:22 +0300

    Выполнил задание №34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите размер массива: ");
+int sizeArray = int.Parse(ReadLine());
+int[] myArray = GetArray(sizeArray, 100, 999);
+// WriteLine($"[{String.Join(",", myArray)}]"); напечатать массив
+WriteLine($"Количество четных символов в массиве - {GetCountEven(myArray)}");
+
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+return result;
+}
+
+int GetCountEven(int[] Array)
+{
+    int count = 0;
+    foreach (var item in Array)
+    {
+        if (item % 2 == 0) count++;
+    }
+    return count;
+}
