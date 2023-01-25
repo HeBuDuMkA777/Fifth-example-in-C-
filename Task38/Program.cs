bb39e02ce7d9d0bd107e22e79de631c0352a23cd
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-25 16:14:29 +0300

    Выполнил задание №38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int sizeArray = 10; // Размер массивав
+int leftRange = -999; //начальная граница массива
+int rightRange = 999; // конечная граница массива
+int[] myArray = GetArray(sizeArray, leftRange, rightRange);
+WriteLine($"[{String.Join(",", myArray)}]"); //напечатать массив
+WriteLine($" Разница между максимальным и минимальным значениями массива равна --> {difference(myArray, leftRange, rightRange)}");
+
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+return result;
+}
+
+int difference(int[] Array, int min, int max)
+{
+    int result = 0;
+    int Maximum = min;
+    int Minimum = max;
+    foreach (var item in Array)
+    {
+        if (item > Maximum) Maximum = item;
+        if (item < Minimum) Minimum = item;
+    }
+    result = Math.Abs(Maximum - Minimum);
+    return result;
+}
