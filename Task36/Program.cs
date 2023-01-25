48a89d678ac76b1043a36cd45f92b81eebde308e
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-25 15:54:46 +0300

    выполнил задание №36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] myArray = GetArray(10, -999, 999);
+WriteLine($"[{String.Join(",", myArray)}]"); //напечатать массив
+WriteLine($" Сумма чисел на четных позициях равна --> {GetSumEven(myArray)}");
+
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+return result;
+}
+
+int GetSumEven(int[] Array)
+{
+    int result = 0;
+    for (int i = 1; i < Array.Length; i += 2)
+    {
+        result += Array[i];
+    }
+    return result;
+}
